1174710e691a40a45382592483579a40ee9e99a7
t.kaneko <nontan20xx@gmail.com>
2017-08-31 19:14:38 +0900

    Update indices to indivi stock option

--- stock_avg.py
+++ stock_avg.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import sys
 import numpy
 import pandas
 import matplotlib.pyplot as plt
@@ -7,6 +8,8 @@ from sklearn import preprocessing
 from keras.models import Sequential
 from keras.layers.core import Dense, Activation
 from keras.layers.recurrent import LSTM
+from keras.optimizers import RMSprop
+from keras.callbacks import EarlyStopping, CSVLogger
 
 class Prediction :
 
@@ -14,7 +17,6 @@ class Prediction :
     self.length_of_sequences = 10
     self.in_out_neurons = 1
     self.hidden_neurons = 300
-
 
   def load_data(self, data, n_prev=10):
     X, Y = [], []
@@ -25,23 +27,33 @@ class Prediction :
     retY = numpy.array(Y)
     return retX, retY
 
-
   def create_model(self) :
     model = Sequential()
-    model.add(LSTM(self.hidden_neurons, \
-              batch_input_shape=(None, self.length_of_sequences, self.in_out_neurons), \
-              return_sequences=False))
+    lstm = LSTM(self.hidden_neurons,
+                batch_input_shape=(None,
+                                    self.length_of_sequences,
+                                    self.in_out_neurons),
+                return_sequences=False)
+
+    model.add(lstm)
     model.add(Dense(self.in_out_neurons))
     model.add(Activation("linear"))
-    model.compile(loss="mape", optimizer="adam")
+    model.compile(loss="mean_squared_error",
+                  optimizer=RMSprop())
     return model
 
 
   def train(self, X_train, y_train) :
     model = self.create_model()
+    csv_logger = CSVLogger('stock_train.log')
     # 学習
-    model.fit(X_train, y_train, batch_size=10, nb_epoch=100)
-    return model
+    hist = model.fit(X_train, y_train,
+              batch_size=10,
+              epochs=100,
+              validation_split=0.2,
+              callbacks=[csv_logger])
+
+    return model, hist
 
 if __name__ == "__main__":
 
@@ -49,11 +61,11 @@ if __name__ == "__main__":
 
   # データ準備
   data = None
-  for year in range(2007, 2017):
-    data_ = pandas.read_csv('csv/indices_I101_1d_' + str(year) +  '.csv', header=None)
+  for year in range(2013, 2017):
+    data_ = pandas.read_csv('csv/stocks_1376-T_1d_' + str(year) +  '.csv', header=None)
     data = data_ if (data is None) else pandas.concat([data, data_])
 
-  data.columns = ['date', 'open', 'high', 'low', 'close']
+  data.columns = ['date', 'open', 'high', 'low', 'close', 'yield', 'sales_value']
   data['date'] = pandas.to_datetime(data['date'], format='%Y-%m-%d')
 
   # 終値のデータを標準化
@@ -67,11 +79,27 @@ if __name__ == "__main__":
   x_train, y_train = prediction.load_data(data[['close']].iloc[0:split_pos], prediction.length_of_sequences)
   x_test,  y_test  = prediction.load_data(data[['close']].iloc[split_pos:], prediction.length_of_sequences)
 
-  model = prediction.train(x_train, y_train)
-  predicted = model.predict(x_test)
-  result = pandas.DataFrame(predicted)
-  result.columns = ['predict']
-  result['actual'] = y_test
-  result.plot()
+  (model, hist) = prediction.train(x_train, y_train)
+  model_json_str = model.to_json()
+  open('stock_model.json', 'w').write(model_json_str)
+  model.save_weights('stock_model.h5');
+
+  loss = hist.history['loss']
+  val_loss = hist.history['val_loss']
+
+  epochs = len(loss)
+  plt.plot(range(epochs), loss, marker='.', label='loss')
+  plt.plot(range(epochs), val_loss, marker='.', label='val_acc')
+  plt.legend(loc='best', fontsize=10)
+  plt.grid()
+  plt.xlabel('epoch')
+  plt.ylabel('loss')
   plt.show()
+
+  # predicted = model.predict(x_test)
+  # result = pandas.DataFrame(predicted)
+  # result.columns = ['predict']
+  # result['actual'] = y_test
+  # result.plot()
+
 
